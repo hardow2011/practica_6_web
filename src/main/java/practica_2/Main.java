2f7b5b12f11f1988313df1bca8236533a73b9e45
hardow2011 <hardow2011@outlook.com>
2020-06-18 12:09:56 -0400

    Clase principal renombrada de App a Main y agregada a mainClassName= 'practica_2.Main'

--- src/main/java/practica_2/Main.java
+++ src/main/java/practica_2/Main.java
@@ -3,12 +3,12 @@
  */
 package practica_2;
 
-public class App {
+public class Main {
     public String getGreeting() {
         return "Hello world.";
     }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        System.out.println(new Main().getGreeting());
     }
 }
