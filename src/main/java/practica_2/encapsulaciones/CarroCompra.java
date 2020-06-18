27576b22d3827ae4cec1c9a2ea92c56406204384
hardow2011 <hardow2011@outlook.com>
2020-06-18 08:13:56 -0400

    Clase CarroCompra creada.

---
+++ src/main/java/practica_2/encapsulaciones/CarroCompra.java
@@ -0,0 +1,45 @@
+package practica_2.encapsulaciones;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarroCompra {
+    
+    private int id;
+    private List<Producto> listaProductos;
+    private int cantidad;
+    private static int contadorIdCarroCompra = 0;
+
+    public CarroCompra(){
+        listaProductos = new ArrayList<>();
+        this.id = contadorIdCarroCompra;
+        this.cantidad = 0;
+        contadorIdCarroCompra++;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    // public void setId(int id) {
+    //     this.id = id;
+    // }
+
+    public List<Producto> getListaProductos() {
+        return listaProductos;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public void insertarProducto(Producto producto) {
+        listaProductos.add(producto);
+        cantidad++;
+    }
+
+}
