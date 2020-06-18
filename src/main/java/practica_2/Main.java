d7dccf4d1e6b771f240892e9266c8d92fe9739db
hardow2011 <hardow2011@outlook.com>
2020-06-18 16:18:09 -0400

    Servidor instanciado y controlador Login habilitado. carpeta /publico agregada a los archivos estaticos.

--- src/main/java/practica_2/Main.java
+++ src/main/java/practica_2/Main.java
@@ -3,12 +3,21 @@
  */
 package practica_2;
 
+import io.javalin.Javalin;
+import io.javalin.core.util.RouteOverviewPlugin;
+import io.javalin.http.staticfiles.Location;
+import practica_2.controladores.*;
+
 public class Main {
-    public String getGreeting() {
-        return "Hello world.";
-    }
 
     public static void main(String[] args) {
-        System.out.println(new Main().getGreeting());
+        // System.out.println(new Main().getGreeting());
+        Javalin app = Javalin.create(config ->{
+            // Si la carpeta /publico no tiene ningún archivo, el build de Gradle fallará.
+             config.addStaticFiles("/publico");
+            // config.registerPlugin(new RouteOverviewPlugin("/rutas"));
+        }).start();
+
+        new LoginControlador(app).aplicarRutas();
     }
 }
