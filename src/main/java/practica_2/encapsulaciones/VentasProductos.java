22e1e863278f1a846e291a4d51ff795e61586246
hardow2011 <hardow2011@outlook.com>
2020-06-18 12:06:27 -0400

    Clase VentaProductos creada.

--- src/main/java/practica_2/encapsulaciones/VentasProductos.java
+++ src/main/java/practica_2/encapsulaciones/VentasProductos.java
@@ -12,7 +12,53 @@ public class VentasProductos {
     private int cantidad;
     private static int contadorIdVentaProductos = 0;
 
-    
+    public VentasProductos(Date fechaCompra, String nombreCliente, List<Producto> listaProductos, int cantidad) {
+        this.id = contadorIdVentaProductos;
+        this.fechaCompra = fechaCompra;
+        this.nombreCliente = nombreCliente;
+        this.listaProductos = listaProductos;
+        this.cantidad = cantidad;
+        contadorIdVentaProductos++;
+    }
 
+    public int getId() {
+        return id;
+    }
+
+    // public void setId(int id) {
+    //     this.id = id;
+    // }
+
+    public Date getFechaCompra() {
+        return fechaCompra;
+    }
+
+    public void setFechaCompra(Date fechaCompra) {
+        this.fechaCompra = fechaCompra;
+    }
+
+    public String getNombreCliente() {
+        return nombreCliente;
+    }
+
+    public void setNombreCliente(String nombreCliente) {
+        this.nombreCliente = nombreCliente;
+    }
+
+    public List<Producto> getListaProductos() {
+        return listaProductos;
+    }
+
+    public void setListaProductos(List<Producto> listaProductos) {
+        this.listaProductos = listaProductos;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
 
 }
