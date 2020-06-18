8741e638e7df79ee5c20057b869900df6b30b3ba
hardow2011 <hardow2011@outlook.com>
2020-06-18 16:21:26 -0400

    Carpeta controladores creada. Controlador login en proceso

---
+++ src/main/java/practica_2/controladores/LoginControlador.java
@@ -0,0 +1,34 @@
+package practica_2.controladores;
+
+
+import io.javalin.Javalin;
+import practica_2.encapsulaciones.Usuario;
+import practica_2.util.BaseControlador;
+// import io.javalin.Javalin;
+import static io.javalin.apibuilder.ApiBuilder.*;
+
+public class LoginControlador extends BaseControlador {
+
+    public LoginControlador(Javalin app) {
+        super(app);
+    }
+
+    @Override
+    public void aplicarRutas() {
+
+        app.routes(() -> {
+
+            path("/", () -> {
+                before("/", ctx -> {
+                    Usuario usuario = ctx.sessionAttribute("usuario");
+                    if(usuario == null){
+                        ctx.redirect("/login.html");
+                    }
+                });
+            });
+        });
+
+
+    }
+    
+}
